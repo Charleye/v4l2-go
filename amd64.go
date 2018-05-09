924f1d988e8832346366a1e1d00bf7cf808a77fe
Charleye <2262757478@qq.com>
2018-05-09 10:12:10 +0800

    first commit

---
+++ amd64.go
@@ -0,0 +1,27 @@
+// +build linux
+// +build amd64
+
+/////////////////////////////////////////////////////
+//                                                 //
+//  !!! THIS IS A GENERATED FILE, DO NOT EDIT !!!  //
+//                                                 //
+/////////////////////////////////////////////////////
+
+package main
+
+const (
+	offset_format_type = 0
+	offset_streamparm_type  = 0
+	offset_requestbuffers_type   = 4
+	offset_buffer_type      = 4
+	offset_cropcap_type        = 0
+	offset_crop_type = 0
+	offset_fmtdesc_type        = 4
+	offset_frmsizeenum_type        = 8
+	offset_frmivalenum_type        = 16
+	offset_queryctrl_type         = 4
+	offset_input_type         = 36
+	offset_output_type         = 36
+	offset_selection_type    = 0
+)
+
---
+++ arch_go.c
@@ -0,0 +1,55 @@
+// Package v4l2, a facade to the Video4Linux2 video capture interface
+// Copyright (C) 2016 Zoltán Korándi <korandi.z@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <linux/videodev2.h>
+
+int main() {
+	printf("// +build linux\n");
+	printf("// +build ");
+	fflush(stdout);
+	system("go env GOHOSTARCH");
+	printf("\n");
+
+	printf("/////////////////////////////////////////////////////\n");
+	printf("//                                                 //\n");
+	printf("//  !!! THIS IS A GENERATED FILE, DO NOT EDIT !!!  //\n");
+	printf("//                                                 //\n");
+	printf("/////////////////////////////////////////////////////\n");
+	printf("\n");
+
+	printf("package main\n\n");
+
+	printf("const (\n");
+	printf("\toffset_format_type = %llu\n", (long long unsigned) offsetof(struct v4l2_format, type));
+	printf("\toffset_streamparm_type  = %llu\n", (long long unsigned) offsetof(struct v4l2_streamparm, type));
+	printf("\toffset_requestbuffers_type   = %llu\n", (long long unsigned) offsetof(struct v4l2_requestbuffers, type));
+	printf("\toffset_buffer_type      = %llu\n", (long long unsigned) offsetof(struct v4l2_buffer, type));
+	printf("\toffset_cropcap_type        = %llu\n", (long long unsigned) offsetof(struct v4l2_cropcap, type));
+	printf("\toffset_crop_type = %llu\n", (long long unsigned) offsetof(struct v4l2_crop, type));
+	printf("\toffset_fmtdesc_type        = %llu\n", (long long unsigned) offsetof(struct v4l2_fmtdesc, type));
+	printf("\toffset_frmsizeenum_type        = %llu\n", (long long unsigned) offsetof(struct v4l2_frmsizeenum, type));
+	printf("\toffset_frmivalenum_type        = %llu\n", (long long unsigned) offsetof(struct v4l2_frmivalenum, type));
+	printf("\toffset_queryctrl_type         = %llu\n", (long long unsigned) offsetof(struct v4l2_queryctrl, type));
+	printf("\toffset_input_type         = %llu\n", (long long unsigned) offsetof(struct v4l2_input, type));
+	printf("\toffset_output_type         = %llu\n", (long long unsigned) offsetof(struct v4l2_output, type));
+	printf("\toffset_selection_type    = %llu\n", (long long unsigned) offsetof(struct v4l2_selection, type));
+	printf(")\n\n");
+
+	return 0;
+}
