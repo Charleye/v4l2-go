a9b8d59eaa403df2784eebfb39f8f3a6cb08ba42
Charleye <2262757478@qq.com>
2018-05-09 10:11:50 +0800

    add single-plannar format test

---
+++ single_plannar_format_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"syscall"
+)
+
+var device = flag.String("d", "/dev/video0", "video device")
+
+func main() {
+	flag.Parse()
+	fd, err := syscall.Open(*device, syscall.O_RDWR, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var vc V4L2_Capability
+	err = IoctlQueryCap(fd, &vc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Driver: %s\n", vc.Driver)
+	fmt.Printf("Card: %s\n", vc.Card)
+	fmt.Printf("BusInfo: %s\n", vc.BusInfo)
+	fmt.Printf("Version: %#x\n", vc.Version)
+	fmt.Printf("Capabilities: %#x\n", vc.Capabilities)
+	fmt.Println("")
+
+	for i := 0; ; i++ {
+		vf := V4L2_Fmtdesc{
+			Index: uint32(i),
+			Type:  V4L2_BUF_TYPE_VIDEO_CAPTURE,
+		}
+		err := IoctlEnumFmt(fd, &vf)
+		if err != nil {
+			if err == syscall.EINVAL {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Println("Description: ", vf.Description)
+		fmt.Printf("PixelFormat: %#x\n", vf.PixelFormat)
+		fmt.Println("Flags: ", vf.Flags)
+	}
+	fmt.Println("")
+
+	var vfmt V4L2_Format
+	var pf V4L2_Pix_Format
+	vfmt.Type = V4L2_BUF_TYPE_VIDEO_CAPTURE
+	vfmt.fmt = &pf
+	err = IoctlGetFmt(fd, &vfmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Width: ", pf.Width)
+	fmt.Println("Height: ", pf.Height)
+	fmt.Printf("PixelFormat: %#x\n", pf.PixelFormat)
+	fmt.Println("SizeImage: ", pf.SizeImage)
+	fmt.Println("Priv: ", pf.Priv)
+	fmt.Println("BytesPerLine: ", pf.BytesPerLine)
+	fmt.Println("")
+
+	vpf := V4L2_Pix_Format{
+		Width:       1024,
+		Height:      768,
+		PixelFormat: GetFourCCByName("YUYV"),
+	}
+	vfmt.fmt = &vpf
+	err = IoctlSetFmt(fd, &vfmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Width: ", vpf.Width)
+	fmt.Println("Height: ", vpf.Height)
+	fmt.Printf("PixelFormat: %#x\n", vpf.PixelFormat)
+	fmt.Println("SizeImage: ", vpf.SizeImage)
+	fmt.Println("Priv: ", vpf.Priv)
+	fmt.Println("BytesPerLine: ", vpf.BytesPerLine)
+}
