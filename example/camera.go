8fda904e52cb9519d99eaa04deb745273b0dec35
Charleye <2262757478@qq.com>
2018-05-28 10:59:32 +0800

    captrue image

--- example/camera.go
+++ example/camera.go
@@ -6,11 +6,17 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 
 	"github.com/Charleye/v4l2-go"
 )
 
 var device = flag.String("d", "/dev/video0", "camera device")
+var image = flag.Bool("i", false, "store frame into image file")
+var fourcc = flag.String("f", "", "set pixel format")
+var videoname = flag.String("v", "", "video name")
+
+var file *os.File
 
 func main() {
 	flag.Parse()
@@ -28,28 +34,36 @@ func main() {
 
 	cam.Width = 800
 	cam.Height = 600
-	cam.PixFmtDescription = "MJPG"
+	cam.PixelFormat = v4l2.GetFourCCByName(*fourcc)
 	cam.SetFormat()
 	cam.AllocBuffers(4)
 	cam.TurnOn()
 	defer cam.TurnOff()
 
-	file, err := os.OpenFile("test.mjpg", os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		log.Fatal(err)
+	if !*image {
+		file, _ = os.OpenFile(*videoname, os.O_RDWR|os.O_CREATE, 0644)
+		defer file.Close()
 	}
-	defer file.Close()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
+	var counter int
 	for {
 		select {
 		case <-c:
 			return
 		default:
+			if *image {
+				imagename := "in" + strconv.Itoa(counter) + "_" + *fourcc + "_800_600.raw"
+				file, _ = os.OpenFile(imagename, os.O_RDWR|os.O_CREATE, 0644)
+			}
 			data := cam.Capture()
 			n, _ := file.Write(data)
 			fmt.Println("Write: ", n)
+			if *image {
+				counter++
+				file.Close()
+			}
 		}
 	}
 }
